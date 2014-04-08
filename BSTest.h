64157789f06d20a1d9374a673281adefcc96488c
Brady Simmons <brady@Brady-Simmonss-MacBook.local>
2014-04-08 11:45:09 -0500

    added a test class

---
+++ BSTest.h
@@ -0,0 +1,13 @@
+//
+//  BSTest.h
+//  Git Test
+//
+//  Created by Brady Simmons on 4/8/14.
+//  Copyright (c) 2014 Lorux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSTest : NSObject
+
+@end
---
+++ BSTest.m
@@ -0,0 +1,13 @@
+//
+//  BSTest.m
+//  Git Test
+//
+//  Created by Brady Simmons on 4/8/14.
+//  Copyright (c) 2014 Lorux. All rights reserved.
+//
+
+#import "BSTest.h"
+
+@implementation BSTest
+
+@end
