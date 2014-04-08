70cf6471972b215da27771733af427221ef0c1ba
Brady Simmons <brady@Brady-Simmonss-MacBook.local>
2014-04-08 11:20:46 -0500

    Initial Commit

---
+++ Git Test/BSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSAppDelegate.h
+//  Git Test
+//
+//  Created by Brady Simmons on 4/8/14.
+//  Copyright (c) 2014 Lorux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/BSViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSViewController.h
+//  Git Test
+//
+//  Created by Brady Simmons on 4/8/14.
+//  Copyright (c) 2014 Lorux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSViewController : UIViewController
+
+@end
